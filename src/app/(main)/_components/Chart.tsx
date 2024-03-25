dcf485340783e5ef4b7036b5e70af1397cf4eba1
YOON-CC <whdbscks77@gmail.com>
2024-03-25 16:57:20 +0900

    feat: chartjs 적용 (#7)

--- src/app/(main)/_components/Chart.tsx
+++ src/app/(main)/_components/Chart.tsx
@@ -5,134 +5,114 @@ import Chart from "chart.js";
 
 export default function CardLineChart() {
   useEffect(() => {
-    var config: any = {
-      type: "line",
-      data: {
-        labels: [
-          "1월",
-          "2월",
-          "3월",
-          "4월",
-          "5월",
-          "6월",
-          "7월",
-          "8월",
-          "9월",
-          "10월",
-          "11월",
-          "12월",
-        ],
-        datasets: [
+    const data = {
+      labels: [
+        "1월",
+        "2월",
+        "3월",
+        "4월",
+        "5월",
+        "6월",
+        "7월",
+        "8월",
+        "9월",
+        "10월",
+        "11월",
+        "12월",
+      ],
+      datasets: [
+        {
+          label: new Date().getFullYear().toString(),
+          backgroundColor: "#ff6c6f",
+          borderColor: "#ff6c6f",
+          data: [65, 78, 66, 44, 56, 67, 75, 65, 78, 66, 44, 56, 67, 75],
+          fill: false,
+        },
+        {
+          label: (new Date().getFullYear() - 1).toString(),
+          backgroundColor: "grey",
+          borderColor: "grey",
+          data: [40, 68, 86, 74, 56, 60, 87, 56, 67, 75, 65, 78, 66, 44],
+          fill: false,
+        },
+      ],
+    };
+
+    const options: any = {
+      maintainAspectRatio: true,
+      responsive: true,
+      title: { display: false },
+      legend: { labels: { fontColor: "grey" }, position: "bottom" },
+      tooltips: { mode: "index", intersect: false },
+      hover: { mode: "nearest", intersect: true },
+      scales: {
+        xAxes: [
           {
-            label: new Date().getFullYear().toString(),
-            backgroundColor: "#3182ce",
-            borderColor: "#3182ce",
-            data: [65, 78, 66, 44, 56, 67, 75],
-            fill: false,
-          },
-          {
-            label: (new Date().getFullYear() - 1).toString(),
-            fill: false,
-            backgroundColor: "#edf2f7",
-            borderColor: "#edf2f7",
-            data: [40, 68, 86, 74, 56, 60, 87],
+            ticks: { fontColor: "grey" },
+            display: true,
+            gridLines: {
+              display: false,
+              borderDash: [2],
+              borderDashOffset: [2],
+              color: "grey",
+              zeroLineColor: "grey",
+              zeroLineBorderDash: [2],
+              zeroLineBorderDashOffset: [2],
+            },
           },
         ],
-      },
-      options: {
-        maintainAspectRatio: false,
-        responsive: true,
-        title: {
-          display: false,
-          text: "Sales Charts",
-          fontColor: "white",
-        },
-        legend: {
-          labels: {
-            fontColor: "white",
+        yAxes: [
+          {
+            ticks: { display: false },
+            display: true,
+            gridLines: {
+              borderDash: [3],
+              borderDashOffset: [3],
+              drawBorder: false,
+              color: "grey",
+              zeroLineColor: "grey",
+              zeroLineBorderDash: [2],
+              zeroLineBorderDashOffset: [2],
+            },
           },
-          position: "bottom",
-        },
-        tooltips: {
-          mode: "index",
-          intersect: false,
-        },
-        hover: {
-          mode: "nearest",
-          intersect: true,
-        },
-        scales: {
-          xAxes: [
-            {
-              ticks: {
-                fontColor: "rgba(255,255,255,.7)",
-              },
-              display: true,
-              scaleLabel: {
-                display: false,
-                labelString: "Month",
-                fontColor: "white",
-              },
-              gridLines: {
-                display: false,
-                borderDash: [2],
-                borderDashOffset: [2],
-                color: "rgba(33, 37, 41, 0.3)",
-                zeroLineColor: "rgba(0, 0, 0, 0)",
-                zeroLineBorderDash: [2],
-                zeroLineBorderDashOffset: [2],
-              },
-            },
-          ],
-          yAxes: [
-            {
-              ticks: {
-                fontColor: "rgba(255,255,255,.7)",
-              },
-              display: true,
-              scaleLabel: {
-                display: false,
-                labelString: "Value",
-                fontColor: "white",
-              },
-              gridLines: {
-                borderDash: [3],
-                borderDashOffset: [3],
-                drawBorder: false,
-                color: "rgba(255, 255, 255, 0.15)",
-                zeroLineColor: "rgba(33, 37, 41, 0)",
-                zeroLineBorderDash: [2],
-                zeroLineBorderDashOffset: [2],
-              },
-            },
-          ],
-        },
+        ],
       },
     };
-    var canvas = document.getElementById("line-chart") as HTMLCanvasElement;
-    if (canvas) {
-      var ctx = canvas.getContext("2d");
-      if (ctx) {
-        new Chart(ctx, config);
+
+    const drawChart = () => {
+      const canvas = document.getElementById("line-chart") as HTMLCanvasElement;
+      if (canvas) {
+        const ctx = canvas.getContext("2d");
+        if (ctx) {
+          new Chart(ctx, { type: "line", data, options });
+        }
       }
-    }
+    };
+
+    drawChart();
   }, []);
   return (
     <>
-      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-blueGray-700">
-        <div className="rounded-t mb-0 px-4 py-3 bg-transparent">
-          <div className="flex flex-wrap items-center">
-            <div className="relative w-full max-w-full flex-grow flex-1">
-              <h1 className="text-white text-xl font-semibold">User Rate</h1>
-            </div>
-          </div>
-        </div>
-        <div className="p-4 flex-auto">
-          {/* Chart */}
-          <div className="relative h-350-px">
-            <canvas id="line-chart"></canvas>
-          </div>
-        </div>
+      <div
+        style={{
+          background: "#ff6c6f",
+          width: "100px",
+          color: "white",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "3px",
+          borderRadius: "5px",
+          marginTop: "3vh",
+          boxShadow: "0px 0px 5px rgba(74, 74, 74, 0.5)",
+        }}
+      >
+        User Rate
+      </div>
+
+      {/* Chart */}
+      <div className="relative h-350-px" style={{ marginTop: "10px" }}>
+        <canvas id="line-chart" height="50"></canvas>
       </div>
     </>
   );
