fe154dfa824dcb6cd64433ebfea026dea4e49b99
YOON-CC <whdbscks77@gmail.com>
2024-03-26 18:45:12 +0900

    feat: 이벤트 베너 구현 (#9)

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,114 @@
 import styles from "@/styles/main.module.css";
+import Image from "next/image";
 import Chart from "@/app/(main)/_components/Chart";
+import event from "@/assets/image/events/event.png";
 
 export default function Home() {
   return (
     <main className={styles.mainPageLeftBody}>
       <Chart />
+
+      <div
+        style={{
+          background: "#ff6c6f",
+          width: "140px",
+          color: "white",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "3px",
+          borderRadius: "5px",
+          marginTop: "3vh",
+          boxShadow: "0px 0px 5px rgba(74, 74, 74, 0.5)",
+        }}
+      >
+        진행중인 이벤트
+      </div>
+      <div
+        className={styles.xScroll}
+        style={{
+          display: "flex",
+          width: "100%",
+          overflowX: "auto",
+          whiteSpace: "nowrap",
+        }}
+      >
+        <div style={{ width: "fit-content", marginTop: "2vh" }}>
+          <div
+            style={{
+              width: "100%",
+              height: "auto",
+              display: "inline-block",
+            }}
+          >
+            <Image
+              src={event}
+              alt=""
+              style={{
+                width: "230px",
+                height: "120px",
+                display: "inline-block",
+                borderRadius: "5px",
+              }}
+            />
+            <Image
+              src={event}
+              alt=""
+              style={{
+                width: "230px",
+                height: "120px",
+                marginLeft: "12px",
+                display: "inline-block",
+                borderRadius: "5px",
+              }}
+            />
+            <Image
+              src={event}
+              alt=""
+              style={{
+                width: "230px",
+                height: "120px",
+                marginLeft: "12px",
+                display: "inline-block",
+                borderRadius: "5px",
+              }}
+            />
+            <Image
+              src={event}
+              alt=""
+              style={{
+                width: "230px",
+                height: "120px",
+                marginLeft: "12px",
+                display: "inline-block",
+                borderRadius: "5px",
+              }}
+            />
+            <Image
+              src={event}
+              alt=""
+              style={{
+                width: "230px",
+                height: "120px",
+                marginLeft: "12px",
+                display: "inline-block",
+                borderRadius: "5px",
+              }}
+            />
+            <Image
+              src={event}
+              alt=""
+              style={{
+                width: "230px",
+                height: "120px",
+                marginLeft: "12px",
+                display: "inline-block",
+                borderRadius: "5px",
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
