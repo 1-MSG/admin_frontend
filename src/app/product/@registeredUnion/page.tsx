f5902c7da94d4158fe62efd390aca0a151f0279d
YOON-CC <whdbscks77@gmail.com>
2024-04-01 14:03:38 +0900

    feat: 상품 등록 회사 데이터 패칭 완료 (#34)

--- src/app/product/@registeredUnion/page.tsx
+++ src/app/product/@registeredUnion/page.tsx
@@ -1,18 +1,27 @@
 import styles from "@/styles/product.module.css";
 
-export default function Page() {
-  const companyNames = Array.from(
-    { length: 50 },
-    (_, index) => `company name${index + 1}`
+async function getProductBrand() {
+  const res = await fetch(
+    "https://gist.githubusercontent.com/YOON-CC/c9ab550e8f4b83c34db60bec462c6f6c/raw/98b235f0b4298016d9b2e66ff6cb8f1175ac2ca1/brandList.json"
   );
+  if (!res.ok) {
+    throw new Error("Network Error");
+  }
+  const data = await res.json();
+
+  return data;
+}
+
+export default async function Page() {
+  const brand = await getProductBrand();
   return (
     <>
       <div className={styles.productPageRightBottomLayout}>
         <div className={styles.productPageRightBottomTitle}>상품 등록 회사</div>
         <div className={styles.productPageRightBottomContainer}>
           <div className={styles.productPageRightBottomContainerList}>
-            {companyNames.map((companyName, index) => (
-              <div key={index}>{companyName}</div>
+            {brand.map((brand: any, index: any) => (
+              <div key={index}>{brand.name}</div>
             ))}
           </div>
         </div>
